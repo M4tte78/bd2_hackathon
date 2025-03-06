d0fd5b95f3219d584ee9dbe2aec8fe58a529a5e5
mcnoun <clairmanon@hotmail.com>
2025-03-06 10:04:26 +0100

    Mise à jour du design

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Controller\Admin;
 
@@ -9,32 +9,56 @@ use App\Entity\Employe;
 use App\Entity\Statut;
 use App\Entity\Role;
 use App\Entity\Utilisateur;
+use App\Repository\ChantierRepository;
+use App\Repository\AffectationRepository;
+use App\Repository\EmployeRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use Symfony\Component\Routing\RouterInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DashboardController extends AbstractDashboardController
 {
+    private $chantierRepository;
+    private $affectationRepository;
+    private $employeRepository;
+
+    public function __construct(
+        ChantierRepository $chantierRepository,
+        AffectationRepository $affectationRepository,
+        EmployeRepository $employeRepository
+    ) {
+        $this->chantierRepository = $chantierRepository;
+        $this->affectationRepository = $affectationRepository;
+        $this->employeRepository = $employeRepository;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         $calendarUrl = $this->generateUrl('affectation_calendar');
 
+        $chantiersEnCours = $this->chantierRepository->count(['statut' => 1]);
+        $chantiersEnAttente = $this->chantierRepository->count(['statut' => 2]);
+        $employesActifs = $this->employeRepository->count([]);
+
         return $this->render('admin/dashboard.html.twig', [
             'calendar_url' => $calendarUrl,
+            'chantiers_en_cours' => $chantiersEnCours,
+            'chantiers_en_attente' => $chantiersEnAttente,
+            'employes_actifs' => $employesActifs,
         ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-        ->setTitle('TEAMBUILD')
-        ->renderContentMaximized()
-        ->disableDarkMode();
+            ->setTitle('TEAMBUILD')
+            ->renderContentMaximized()
+            ->disableDarkMode();
     }
 
     public function configureAssets(): Assets
